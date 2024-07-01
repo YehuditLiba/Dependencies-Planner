f56cd884ae2de695d780666df9ba8e684a82a93f
YehuditLiba <Y4144246@GMAIL.COM>
2024-07-01 16:51:39 +0300

    request type

---
+++ BackeEnd/src/types/requestTypes.ts
@@ -0,0 +1,20 @@
+enum Priority {
+    Low = 'Low',
+    Medium = 'Medium',
+    High = 'High',
+    Critical = 'Critical'
+}
+
+interface Request {
+    ID: number;
+    title: string;
+    requestGroup: string[];
+    description: string;
+    priority: Priority;
+    finalDecision: boolean;
+    planned: string;
+    comments: string;
+    dateTime: Date;
+    affectedGroupList: AffectedGroup[];
+    jiraLink: string;
+}
