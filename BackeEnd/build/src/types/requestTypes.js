b82c23e67bdb74f7ff0dbfd7875d4f7ce8ff7e37
YehuditLiba <Y4144246@GMAIL.COM>
2024-07-09 16:23:50 +0300

    Connecting to the remote server

---
+++ BackeEnd/build/src/types/requestTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Priority;
+(function (Priority) {
+    Priority["Low"] = "Low";
+    Priority["Medium"] = "Medium";
+    Priority["High"] = "High";
+    Priority["Critical"] = "Critical";
+})(Priority || (Priority = {}));
--- BackeEnd/src/types/productManagerTypes.ts
+++ BackeEnd/src/types/productManagerTypes.ts
@@ -3,3 +3,21 @@ export interface ProductManager {
     name: string;
     groupID: number[];
 }
+
+
+import { pool } from '../config/db';
+import { poolG } from '../config/db';
+
+export default class ProductManagerRepo {
+    static async getAllProductManagerNames(): Promise<{ name: string }[]> {
+        try {
+            console.log('Entering getAllProductManagerNames method');
+            const result = await pool.query('SELECT name FROM productmanager');
+            console.log('Query executed successfully, result:', result.rows);
+            return result.rows;
+        } catch (err) {
+            console.error('Error executing query in getAllProductManagerNames:', err);
+            throw err;
+        }
+    }
+}
