a1d7c349d0296718528c5d9e3d9660a8005d1402
אביטל-פרץ <aaappp05567@gmail.com>
2024-07-21 16:59:45 +0300

    Login page

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
-import MainTable from './components/MainTable';
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/Login'; // הנתיב לקובץ LoginPage.jsx
+import MainTable from './components/MainTable'; // הנתיב לקובץ MainTable.jsx
+
+const App = () => {
   return (
-    <MainTable/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/MainTable" element={<MainTable />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
