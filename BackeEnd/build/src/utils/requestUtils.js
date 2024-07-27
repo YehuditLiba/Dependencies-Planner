9a9a13fdcaff097b3d9d0ac63b2adbad770b51f3
MichalFremd1 <m0583227176@gmail.com>
2024-07-27 22:33:21 +0300

    Save local changes before merging master

--- BackeEnd/build/src/utils/requestUtils.js
+++ BackeEnd/build/src/utils/requestUtils.js
@@ -241,28 +241,6 @@ const updateFinalDecision = (id, finalDecision) => __awaiter(void 0, void 0, voi
     }
 });
 exports.updateFinalDecision = updateFinalDecision;
-//הוספת בקשה חדשה
-// export const addRequest = async (request: RequestT): Promise<void> => {
-//     const query = `
-//       INSERT INTO request (ID, title, request_group, description, priority, planned, comments, date_time, affected_group_list, jira_link, requestor_name,requestor_email)
-//       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
-//     `;
-//     const values = [
-//         request.ID,
-//         request.title,
-//         request.requestGroup,
-//         request.description,
-//         request.priority,
-//         request.planned,
-//         request.comments,
-//         request.dateTime,
-//         request.affectedGroupList,
-//         request.jiraLink,
-//         request.requestorName,
-//         request.emailRequestor,
-//     ];
-//     await pool.query(query, values);
-// };
 const addRequest = (request) => __awaiter(void 0, void 0, void 0, function* () {
     const query = `
       INSERT INTO request ( title, request_group, description, priority, planned, comments, date_time, affected_group_list, jira_link, requestor_name,requestor_email)
