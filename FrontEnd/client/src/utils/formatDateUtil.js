2e83029c25112bf9bd03474df6e34cffe502928e
MichalFremd1 <m0583227176@gmail.com>
2024-07-29 14:46:01 +0300

    merge to master

---
+++ FrontEnd/client/src/utils/formatDateUtil.js
@@ -0,0 +1,13 @@
+export const formatDateTime = (value) => {
+    const date = new Date(value);
+    const options = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    };
+    return date.toLocaleString('he-IL', options);
+  };
+  
