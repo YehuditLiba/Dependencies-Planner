7e84b3f02236883c523b50813d7b07609e823cfe
sara-walden <csw3759@gmail.com>
2024-08-04 15:07:59 +0300

    adding more of Admin settings

--- BackeEnd/src/Controllers/GroupCon..ts
+++ BackeEnd/src/Controllers/GroupCon..ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { getAllGroups,updateGroupById } from '../Utils/GroupUtils';
+import { getAllGroups,updateGroupById, addGroupToDatabase,deleteGroupById  } from '../Utils/GroupUtils';
 
 export const getAllGroupsController = (req: Request, res: Response) => {
     getAllGroups((err, groups) => {
@@ -24,4 +24,28 @@ export const editGroupByAdmin = (req: Request, res: Response) => {
         }
         res.status(200).json({ message: 'Group updated successfully' });
     });
+};
+export const addGroup = (req: Request, res: Response) => {
+    const { name } = req.body;
+
+    console.log('Received new group:', name);
+
+    addGroupToDatabase(name, (err) => {
+        if (err) {
+            console.error('Error adding group:', err);
+            return res.status(500).json({ error: 'Failed to add group' });
+        }
+        res.status(201).json({ message: 'Group added successfully' });
+    });
+};
+export const deleteGroup = async (req: Request, res: Response): Promise<void> => {
+    const groupId = req.params.groupId;
+
+    try {
+        await deleteGroupById(groupId);
+        res.status(200).json({ message: 'Group deleted successfully' });
+    } catch (err) {
+        console.error('Error deleting group:', err);
+        res.status(500).json({ error: 'Failed to delete group' });
+    }
 };
--- BackeEnd/src/routes/routing.ts
+++ BackeEnd/src/routes/routing.ts
@@ -1,22 +1,22 @@
 import express from 'express';
-import { getAllGroupsController, editGroupByAdmin } from '../Controllers/GroupCon.';
+import { getAllGroupsController, editGroupByAdmin, addGroup , deleteGroup} from '../Controllers/GroupCon.';
 import {  getRequestByIdController, updateRequest, 
     deleteRequest, updateRequestByIdController,updateFinalDecisionController ,
     createRequest, updatePlannedField, getAllFilteredRequestsWithPagination} from '../Controllers/requestCon';
-import { getAllProductManagers, getAllRequestsByProductManager, editProductManagerByAdmin } from '../Controllers/productManagerCon';
+import { getAllProductManagers, getAllRequestsByProductManager, editProductManagerByAdmin,addProductManager, getGroupsByProductManager , deleteProductManager } from '../Controllers/productManagerCon';
 import { getAllStatusController, getAllStatus } from '../Controllers/StatusCon';
 import { getAllPrioritiesController, updatePriorityController } from '../Controllers/PriorityCon';
 // import { getAllTSize } from '../Controllers/T_SizeCon';
 // import { getAllDecisionsController } from '../Controllers/final_decisionCon';
-import { getAllAffectedGroupsController, createAffectedGroup, updateAffectedGroupStatus, deleteAffectedGroups, getAllRequestsWithStatusesController 
-    
-} from '../Controllers/affectedGroupCon';
+import { getAllAffectedGroupsController, createAffectedGroup, updateAffectedGroupStatus, deleteAffectedGroups, getAllRequestsWithStatusesController} from '../Controllers/affectedGroupCon';
+import {addProductManagerToGroupHandler,getProductManagerGroupsHandler , getAllProductManagerGroupsHandler} from '../Controllers/productManagerGroupCon';
 
 const router = express.Router();
 //groups routings
 router.get('/groups', getAllGroupsController);
 router.put('/groups/:groupId', editGroupByAdmin);
-
+router.post('/groups', addGroup);
+router.delete('/groups/:groupId', deleteGroup);
 
 //Requests routings
 // router.get('/Allrequests', getAllRequests);
@@ -42,7 +42,15 @@ router.get('/requestsWithStatuses', getAllRequestsWithStatusesController);
 //routings ProductManager
 router.get('/productManagers', getAllProductManagers);
 router.get('/requests/:groupId', getAllRequestsByProductManager);
-router.put('/productManagers/:email', editProductManagerByAdmin);
+router.put('/editProductManagers/:email', editProductManagerByAdmin);
+router.post('/addProductManagers', addProductManager);
+router.get('/product-managers/:email/groups', getGroupsByProductManager);
+router.delete('/productManagers/:email', deleteProductManager);
+
+//routings ProductManagerGroups 
+router.post('/product-manager-group', addProductManagerToGroupHandler);
+router.get('/product-manager-group/:email', getProductManagerGroupsHandler);
+router.get('/all-product-manager-groups', getAllProductManagerGroupsHandler); 
 //status routings
 router.get('/status', getAllStatusController);
 router.get('/Getstatus', getAllStatus);
