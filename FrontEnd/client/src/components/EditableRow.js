df3b9c7dbfc54f92bc35e194a486081c21dcff3b
MichalFremd1 <m0583227176@gmail.com>
2024-08-06 18:40:11 +0300

    turned back show & hide group columns

--- FrontEnd/client/src/components/EditableRow.js
+++ FrontEnd/client/src/components/EditableRow.js
@@ -95,7 +95,7 @@ const EditableRow = ({ row, columns, onSave, emailRequestor,
         const status = row.statuses.find(status => status.groupId === groupId);
         return status ? status.status.status : 'Not Required';
     };
-    
+
 
     return (
         <TableRow
@@ -145,22 +145,20 @@ const EditableRow = ({ row, columns, onSave, emailRequestor,
                     )}
                 </TableCell>
             ))}
-            {/* {groups.map((group) => {
-                // console.log(editData.statuses.find(status => status.groupId === group.id))
-                // const status = editData.statuses.find(status => status.groupId === group.id);
-                const status = (row.statuses || []).find(status => status.groupId === group.id);
-                const statusDescription = status ? status.status.status : 'Not Required';
+            {groups.map((group) => {
+                const statusDescription = getGroupStatus(row, group.id);
                 // הגדרת סגנון התא
                 let cellStyle = {};
                 if (statusDescription === 'Not Required') {
                     cellStyle = { color: 'gray' }; // צבע אפור ל-'Not Required'
                 }
                 return (
+                    showGroupColumns ? (
                     <TableCell
                         key={group.id}
-                        style={{ backgroundColor: getStatusBackgroundColor(getGroupStatus(row, group.id)), ...cellStyle }}
+                        style={{ backgroundColor: getStatusBackgroundColor(statusDescription), ...cellStyle }}
                     >
-                        {/* {isEditing ? (
+                        {isEditing ? (
                             <Select
                                 value={statusDescription}
                                 onChange={(e) => setEditData({ ...editData, [group.id]: e.target.value })}
@@ -173,40 +171,11 @@ const EditableRow = ({ row, columns, onSave, emailRequestor,
                             </Select>
                         ) : (
                             statusDescription
-                        )} */}{/*}
-                        {statusDescription}
+                        )}
                     </TableCell>
+                    ) : null
                 );
-            })} */}
-            {groups.map((group) => {
-    const statusDescription = getGroupStatus(row, group.id);
-    // הגדרת סגנון התא
-    let cellStyle = {};
-    if (statusDescription === 'Not Required') {
-        cellStyle = { color: 'gray' }; // צבע אפור ל-'Not Required'
-    }
-    return (
-        <TableCell
-            key={group.id}
-            style={{ backgroundColor: getStatusBackgroundColor(statusDescription), ...cellStyle }}
-        >
-            {isEditing ? (
-                <Select
-                    value={statusDescription}
-                    onChange={(e) => setEditData({ ...editData, [group.id]: e.target.value })}
-                >
-                    {statuses.map(status => (
-                        <MenuItem key={status.id} value={status.status}>
-                            {status.status}
-                        </MenuItem>
-                    ))}
-                </Select>
-            ) : (
-                statusDescription
-            )}
-        </TableCell>
-    );
-})}
+            })}
 
         </TableRow>
     );
